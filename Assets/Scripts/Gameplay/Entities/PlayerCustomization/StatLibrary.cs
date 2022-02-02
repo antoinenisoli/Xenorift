004cbe1dc068763a36d945547d2b34fb0d160b65
Antoine Nisoli <nisoliantoine@outlook.com>
2022-02-02 16:51:01 +0100

    stat library

--- Assets/Scripts/Gameplay/Entities/PlayerCustomization/StatLibrary.cs
+++ Assets/Scripts/Gameplay/Entities/PlayerCustomization/StatLibrary.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "StatLibrary")]
+[CreateAssetMenu(fileName = "NewStatLibrary", menuName = "Player/Stats/StatLibrary")]
 public class StatLibrary : ScriptableObject
 {
     [SerializeField] Color[] qualityColors = new Color[] { Color.white };
     [SerializeField] StatData[] statData;
+
+    [ContextMenu(nameof(CreateList))]
+    private void CreateList()
+    {
+        System.Array array = System.Enum.GetValues(typeof(PlayerStat));
+        statData = new StatData[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            PlayerStat type = (PlayerStat)array.GetValue(i);
+            StatData data = new StatData();
+            data.stat = type;
+            statData[i] = data;
+        }
+    }
 }
